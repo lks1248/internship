99323b03d6c6e77f52bc064ab3257f65fc75db82
luke_sch02 <lukas.schmidt@gmx.ch>
2022-03-18 10:40:41 +0100

    added support for HDF5

--- evrard_cutter.py
+++ evrard_cutter.py
@@ -1,55 +1,67 @@
 import sys 
+from copy import copy
 import numpy as np
+import h5py 
 from numba import jit 
 
 src = sys.argv[1]
-npart = int(sys.argv[2])
-mpart = 1/npart
+out = src[:-3]
 nneighbours = 100
-
-data = np.fromfile(src)
-data = data.reshape(11, npart)
-print(data)
+h5f = h5py.File(src, 'r')
+x = h5f["Step#0/x"][:]
+y = h5f["Step#0/y"][:]
+z = h5f["Step#0/z"][:]
+npart = len(h5f["Step#0/x"])
+mpart = 1/npart
 
 #cut sphere out of the block
-def cut(data):
-	a = np.empty((11, 0))
+def cut(h5f):
+	a = np.empty((6, 0))
 	print(a)
 	for i in range(npart):
-		radius = (data[0,i]**2 + data[1,i]**2 + data[2,i]**2) ** 0.5
+		
+		radius = (x[i]**2 + y[i]**2 + z[i]**2) ** 0.5
 		
 		#print(radius)
 		if radius <= 1:
 			h = ((2*radius*mpart*nneighbours)/((4/3)*8))**(1/3)
 			#print(data[:,i])
-			b = np.empty(([11,1]))
-			b[0] = data[0,i] * radius**(1/2)
-			b[1] = data[1,i] * radius**(1/2)
-			b[2] = data[2,i] * radius**(1/2)
-			b[3] = 0.0
-			b[4] = 0.0
-			b[5] = 0.0
-			b[6] = 1.0
-			b[7] = 5.000000000000000278e-2
-			b[8] = data[8,i]
+			b = np.empty(([6,1]))
+			b[0] = x[i] * radius**(1/2)
+			b[1] = y[i] * radius**(1/2)
+			b[2] = z[i] * radius**(1/2)
+			b[3] = 5.000000000000000278e-2
 			if h < 0.02:
-				b[9] = 0.02 #constant value for now, should be a function of npart and box size
+				b[4] = 0.02 #constant value for now, should be a function of npart and box size
 			else: 
-				b[9] = h
-			#print(b[9])
-			b[10] = 0.0
-			#print(b)
-			#print("------------")
-			#b = b.reshape(11,1)
-			#print(a.shape)
-			#print(b.shape)
+				b[4] = h
+
+			b[5] = 0.0
+
 			a = np.c_[a, b]
 	return a
 		
-a = cut(data)
-a[10,:] = 1/a.shape[1]
+a = cut(h5f)
+a[5,:] = 1/a.shape[1]
 print(a)
-print(a.shape)
-a.tofile(src+"_evrard")
+
+h5f = h5py.File(out+"_evrard.h5", 'w')
+step0 = h5f.create_group("Step#0") 
+#write data
+step0.create_dataset('x', data=a[0])
+step0.create_dataset('y', data=a[1])
+step0.create_dataset('z', data=a[2])
+step0.create_dataset('h', data=a[4])
+step0.create_dataset('u', data=a[3])
+step0.create_dataset('m', data=a[5])
+#set attributes
+step0.attrs["gravConstant"] = [-1.0]
+step0.attrs["box"] = 	[0.0, 1.0, 0.0, 1.0, 0.0, 1.0]
+step0.attrs["minDt"] = 	[1.0e-4]
+step0.attrs["pbc"] = 	[0, 0, 0]
+step0.attrs["step"] = 	[0]
+step0.attrs["time"] =	[0.0]
+#a.tofile(src+"_evrard")
+h5f.close()
 b = a.transpose()
 np.savetxt(src+"_evrard.txt", b)
--- outputwriter_sphexa.py
+++ outputwriter_sphexa.py
@@ -1,27 +1,41 @@
 import sys
 import numpy as np
+import h5py
 from parser_working import eat_snapshot
+from numba import njit
 
 src = sys.argv[1] #filepath to binary input file
 header, gas, dm = eat_snapshot(src)
-out = src + "_sphexa"
+out = src + "_sphexa.h5"
 gas.remove_columns(['id', 'rhom', 'rho', 'u', 'Bx', 'By', 'Bz'])
-gas.add_column(1.0, name='ro', index=6)
 gas.add_column(0.0, name='u', index=7)
-gas.add_column(0.0, name='p', index=8)
-gas.add_column(0.0, name='m', index=11)
+gas.add_column(1/len(gas), name='m', index=11)
 
 print(gas)
 
-for i in range(len(gas)):
-	gas['x'][i] = gas['x'][i] - 0.5
-	gas['y'][i] = gas['y'][i] - 0.5
-	gas['z'][i] = gas['z'][i] - 0.5
-	gas['hsml'][i] = gas['hsml'][i] / 3 
+gas['x'] = gas['x'] - 1
+gas['y'] = gas['y'] - 1
+gas['z'] = gas['z'] - 1
+gas['hsml'] = gas['hsml'] / 3 
 
 print(gas)
 
 gas.write(out+".txt", format='ascii', overwrite=True)
-data = np.loadtxt(out+".txt", skiprows=1)
-data2 = np.transpose(data)
-data2.tofile(out)
+
+h5f = h5py.File(out, 'w')
+step0 = h5f.create_group("Step#0") 
+#write data
+step0.create_dataset('x', data=gas['x'])
+step0.create_dataset('y', data=gas['y'])
+step0.create_dataset('z', data=gas['z'])
+step0.create_dataset('h', data=gas['hsml'])
+step0.create_dataset('u', data=gas['u'])
+step0.create_dataset('m', data=gas['m'])
+#set attributes
+step0.attrs["box"] = 	[0.0, 1.0, 0.0, 1.0, 0.0, 1.0]
+step0.attrs["minDt"] = 	[1.0e-4]
+step0.attrs["pbc"] = 	[0, 0, 0]
+step0.attrs["step"] = 	[0]
+step0.attrs["time"] =	[0.0]
+h5f.close()
+
